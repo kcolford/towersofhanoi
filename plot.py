6b233e42cabd523dbcc1069a1eea72dfb96713a3
Kieran Colford <colfordk@gmail.com>
2013-10-30 21:34:55 -0400

    Enhanced the plot script to provide better graphing support.

--- plot.py
+++ plot.py
@@ -4,10 +4,25 @@ from test import m
 from sys import argv, stdin
 from os import popen
 
-c = 'plot "-" smooth unique title "{}"'
-c = "gnuplot -e 'set term pstex; " + c + "'"
 
-for a in stdin:
-    with popen(c.format(argv[1]), 'w') as p:
-        for x in range(100):
-            print(x, eval(a), file=p)
+def plot3d():
+    with popen('gnuplot -p', 'w') as p:
+        print('splot "-"', file=p)
+        for k in range(7, 30):
+            for n in range(300):
+                print(k, n, m(k, n), file=p)
+
+def main():
+    c = 'plot "-" smooth unique title "{}"'
+    c = "gnuplot -e 'set term pstex; " + c + "'"
+    for b, a in enumerate(stdin):
+        s = c.format(argv[b + 1 if b + 1 < len(argv) else -1])
+        with popen(s, 'w') as p:
+            for x in range(100):
+                print(x, eval(a), file=p)
+
+if __name__ == '__main__':
+    if __debug__:
+        main()
+    else:
+        plot3d()
