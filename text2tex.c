ee17a174c3ab4d7d9d04a097009efec48edafde1
Kieran Colford <colfordk@gmail.com>
2013-11-04 19:02:07 -0500

    Edited to take advantage of switch tables.

--- text2tex.c
+++ text2tex.c
@@ -1,14 +1,27 @@
 /* This is a program that translates ascii text into plain TeX
    output. */
 
+/* Copyright (C) 2013 Kieran Colford
+
+   This program is free software: you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see
+   <http://www.gnu.org/licenses/> */
+
 #include <stdio.h>
-#include <string.h>
 
 int main ()
 {
-  int zero1 = 0;
-  char c;
-  int zero2 = 0;
+  register char c;
 
   puts ("{\\tt");                     /* Use typewriter font. */
   puts ("\\noindent");
@@ -27,13 +40,27 @@ int main ()
 	  puts ("\\hfill\n");
 	  puts ("\\noindent");
 	  break;
-	default:
+	  /* We type all of these cases out because it gives the
+	     compiler a chance to generate a "switch table" which is
+	     much more efficient than doing some sort search. */
+	case '\\':
+	case '$':
+	case '&':                    
+	case '{':
+	case '}':
+	case '#':
+	case '~':
+	case '`':
+	case '_':
+	case '^':
+	case '"':
+	case '%':
 	  /* Certain characters aren't printed directly in TeX, so we
 	     tell TeX to print them according to their ascii code. */
-	  if (strstr ("\"^~#_%${}&\\", &c))
-	    printf ("\\char%d", c); 
-	  else 
-	    putchar (c);
+	  printf ("\\char%d", c); 
+	  break;
+	default:
+	  putchar (c);
 	  break; 
 	}
     }
